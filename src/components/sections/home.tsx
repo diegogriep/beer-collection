f5c017a735887d26803211d3af4eb299bb4500ec
Diego Griep <fale@diegogriep.com.br>
2024-02-26 22:10:05 +0000

    chore: Suspense react

--- src/components/sections/home.tsx
+++ src/components/sections/home.tsx
@@ -3,11 +3,10 @@
 import { getData } from '@/utils/getData'
 import { getStorageItem } from '@/utils/localStorage'
 import { useSearchParams } from 'next/navigation'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import BeerCard, { BeerProps } from '../BeerCard'
 
 export default function HomeSection() {
-  const [loading, setLoading] = useState(true)
   const [beers, setBeers] = useState<BeerProps[]>([])
   const searchParams = useSearchParams()
   const beer_name = searchParams.get('beer_name')
@@ -15,7 +14,6 @@ export default function HomeSection() {
   useEffect(() => {
     async function fetchData() {
       const result = await getData(beer_name)
-      setLoading(false)
       setBeers(result)
     }
 
@@ -27,9 +25,11 @@ export default function HomeSection() {
     ? beers.concat(checkThereIsNewBottle).reverse()
     : beers
 
-  return loading
-    ? 'loading...'
-    : allBeers.length > 0
-    ? allBeers.map((beer) => <BeerCard key={beer.id} {...beer} />)
-    : 'No beers found'
+  return (
+    <Suspense fallback={'loading...'}>
+      {allBeers.length > 0
+        ? allBeers.map((beer) => <BeerCard key={beer.id} {...beer} />)
+        : 'No beers found'}
+    </Suspense>
+  )
 }
